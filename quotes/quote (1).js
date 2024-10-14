832daff4357cf128b4313b1f88234a802faf7edb
Alex Skiles <69220048+grorthory@users.noreply.github.com>
2024-10-13 20:12:15 -0700

    Add files via upload

---
+++ quotes/quote (1).js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded", function () {
+   document.querySelector("#fetchQuotesBtn").addEventListener("click", function () {
+
+      // Get values from drop-downs
+      const topicDropdown = document.querySelector("#topicSelection");
+      const selectedTopic = topicDropdown.options[topicDropdown.selectedIndex].value;
+      const countDropdown = document.querySelector("#countSelection");
+      const selectedCount = countDropdown.options[countDropdown.selectedIndex].value;
+   
+      // Get and display quotes
+      fetchQuotes(selectedTopic, selectedCount);	   
+   });
+});
+
+function fetchQuotes(topic, count) {
+   // TODO: Modify to use XMLHttpRequest
+
+   let request = new XMLHttpRequest();
+  request.addEventListener("load", responseReceivedHandler);
+  request.responseType="json";
+  request.open("GET", "https://wp.zybooks.com/quotes.php?topic="+topic+"&count="+count);
+  request.send();
+   let html = "<ol>";
+   for (let c = 1; c <= count; c++) {
+      html += `<li>Quote ${c} - Anonymous</li>`;
+   }
+   html += "</ol>";
+
+   document.querySelector("#quotes").innerHTML = html;
+}
+
+// TODO: Add responseReceivedHandler() here
+function responseReceivedHandler(){
+  
+}
